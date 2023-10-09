772fcaaf239ee20468007dc94a059c2ea4825dd7
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-10-08 21:23:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+import streamlit as st
 from googletrans import Translator
 from gtts import gTTS
-import os
 
-frase = input("Ingresa la frase que quieres traducir: ")
-idioma_destino = input("Ingresa el idioma de destino (por ejemplo, 'es' para español): ")
+st.title("Aplicativo AMA")
+st.image("translate.png", width=200)
 
-traductor = Translator()
-traduccion = traductor.translate(frase, dest=idioma_destino)
-frase_traducida = traduccion.text
+st.subheader("Texto a audio y traducción.")
 
-def text_to_speech(text, tld):
-    
-    tts = gTTS(text,"es", tld, slow=False)
-    try:
-        my_file_name = text[0:20]
-    except:
-        my_file_name = "audio"
-    tts.save(f"temp/{my_file_name}.mp3")
-    return my_file_name, text
+text = st.text_input("Ingrese el texto:")
+
+languages = {"Inglés": "en", "Español": "es", "Chino Mandarín": "zh-cn", "Francés": "fr", "Ruso": "ru", "Hindi": "hi"}
+target_lang = st.selectbox("Seleccione el idioma de destino:", list(languages.keys()))
+
+if text and target_lang:
+    source_lang = "es"
+    target_lang_code = languages[target_lang]
+    translator = Translator()
+    translated_text = translator.translate(text, src=source_lang, dest=target_lang_code).text
+
+    tts = gTTS(translated_text, lang=target_lang_code, slow=False)
+    tts.save("temp/audio.mp3")
+
+    audio_file = open("temp/audio.mp3", "rb")
+    audio_bytes = audio_file.read()
+
+    st.markdown("## Tu audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+    st.markdown("## Texto en audio:")
+    st.write(translated_text)
+
+
