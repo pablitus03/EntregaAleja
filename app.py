e1951d331c5c61ca1c802293ede053560bd41144
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-10-08 21:17:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ from googletrans import Translator
 from gtts import gTTS
 import os
 
-# Solicita la frase y el idioma de destino
 frase = input("Ingresa la frase que quieres traducir: ")
 idioma_destino = input("Ingresa el idioma de destino (por ejemplo, 'es' para español): ")
 
-# Crea una instancia del traductor y traduce la frase al idioma de destino
 traductor = Translator()
 traduccion = traductor.translate(frase, dest=idioma_destino)
 frase_traducida = traduccion.text
 
-# Crea una instancia de gTTS para generar el audio
 def text_to_speech(text, tld):
     
     tts = gTTS(text,"es", tld, slow=False)
